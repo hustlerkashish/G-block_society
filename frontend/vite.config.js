20927d4f5a3bc07ec4d47bba8d83ba8c4fcc4c75
KashishGandhi <kashishgandhi.278@gmail.com>
2025-10-05 19:23:27 +0530

    update_vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom']
+        }
+      }
+    }
+  },
+  server: {
+    port: 5173,
+    open: true,
+  }
 })
